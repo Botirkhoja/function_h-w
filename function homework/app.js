f881f23028f7a85034a9d7f7d812972e9a2db65c
Yunus <128239091+Botirkhoja@users.noreply.github.com>
2023-05-27 15:13:11 +0300

    Add files via upload

---
+++ function homework/app.js
@@ -0,0 +1,19 @@
+const misol = function(son1,son2){
+    return son1 % son2
+}
+
+console.log(misol(20,9));
+
+
+function misol2 (raqam, raqam2){
+    return raqam % raqam2
+}
+
+console.log(misol(49,5));
+
+
+const Name = (number1, number2) =>{
+    return number1 % number2
+}
+
+console.log(Name(30,9));
